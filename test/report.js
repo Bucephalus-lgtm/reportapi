bf9354367ffef1f9a440b110f4a9c13b4943c2a4
Bucephalus <bhargabnath691@gmail.com>
2021-08-31 15:57:56 +0530

    test route updated

--- test/report.js
+++ test/report.js
@@ -18,10 +18,10 @@ describe('Reports', () => {
         });
     });
 
-    describe('GET api/report report', () => {
-        it('it should GET a report by the given id', (done) => {
-            let report = new Report({
-                userId: "user-2",
+    describe('/POST report', () => {
+        it('it should POST a report ', (done) => {
+            let report = {
+                userID: "user-2",
                 marketID: "market-1",
                 marketName: "Vashi Navi Mumbai",
                 cmdtyID: "cmdty-1",
@@ -29,31 +29,64 @@ describe('Reports', () => {
                 priceUnit: "Quintal",
                 convFctr: 100,
                 price: 1600
-            });
-            report.save((err, report) => {
-                chai.request(server)
-                    .get('/report')
-                    .query({
-                        reportId: '612d34906461c3a7b2d44e98',
-                    })
-                    .send(report)
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.body.should.be.a('object');
-                        res.body.should.have.property('_id');
-                        res.body.should.have.property('userId');
-                        res.body.should.have.property('marketID');
-                        res.body.should.have.property('marketName');
-                        res.body.should.have.property('cmdtyID');
-                        res.body.should.have.property('cmdtyName');
-                        res.body.should.have.property('priceUnit');
-                        res.body.should.have.property('convFctr');
-                        res.body.should.have.property('price');
-                        res.body.should.have.property('id').eql(report.id);
-                        done();
-                    });
-            });
+            }
+            chai.request(server)
+                .post('/report')
+                .send(report)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('report successfully added!');
+                    res.body.report.should.have.property('userID');
+                    res.body.report.should.have.property('marketID');
+                    res.body.report.should.have.property('marketName');
+                    res.body.report.should.have.property('cmdtyID');
+                    res.body.report.should.have.property('cmdtyName');
+                    res.body.report.should.have.property('marketType');
+                    res.body.report.should.have.property('convFctr');
+                    res.body.report.should.have.property('price');
+                    res.body.report.should.have.property('priceUnit');
+                    done();
+                });
+        });
 
+        describe('GET api/report report', () => {
+            it('it should GET a report by the given id', (done) => {
+                let report = new Report({
+                    userId: "user-2",
+                    marketID: "market-1",
+                    marketName: "Vashi Navi Mumbai",
+                    cmdtyID: "cmdty-1",
+                    cmdtyName: "Potato",
+                    priceUnit: "Quintal",
+                    convFctr: 100,
+                    price: 1600
+                });
+                report.save((err, report) => {
+                    chai.request(server)
+                        .get('/report')
+                        .query({
+                            reportId: '612d34906461c3a7b2d44e98',
+                        })
+                        .send(report)
+                        .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+                            res.body.should.have.property('_id');
+                            res.body.should.have.property('userId');
+                            res.body.should.have.property('marketID');
+                            res.body.should.have.property('marketName');
+                            res.body.should.have.property('cmdtyID');
+                            res.body.should.have.property('cmdtyName');
+                            res.body.should.have.property('priceUnit');
+                            res.body.should.have.property('convFctr');
+                            res.body.should.have.property('price');
+                            res.body.should.have.property('id').eql(report.id);
+                            done();
+                        });
+                });
+
+            });
         });
     });
 });
